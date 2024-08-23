2b6b7f714042e54ee6dd063fcddadde87f21cf02
dsak789 <dsak.official@gmail.com>
2024-08-24 02:40:38 +0530

    AI Gemini Bot

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ if "chats" not in st.session_state:
 #     st.session_state.loop = 0
 # loop = st.session_state.loop
 st.set_page_config( 
-    page_title= "My Bujji",
+    page_title= "Im Bujji",
     page_icon = "💬"
 )
 
 
-st.title("Hi I'm :violet[Bujji]")
-st.write("Small Clone of Gemini API")
+st.title("Hi..! I'm :green[Bujji]")
+st.write(":orange[Single] Response Chat bot")
 # loop = 1
 # for chat in st.session_state.chats:
 #     # st.warning(len(st.session_state.chats))
@@ -35,9 +35,9 @@ if user_query:
     res = model.generate_content(user_query)
     # user_query = None
     print(res)
-    st.warning("This Page and :blue[Bujji's Response] will not be for longer Time \n So You can save page as :red[PDF] by :red[Printing] is you want response ")
+    st.warning("This Page and :blue[Bujji's Response] will not be for longer Time \n So You can save page as :red[PDF] by :red[Printing] If you want response ")
     st.write(f'*:blue[You]* :  {user_query}')
-    st.write(f'*:orange[BUJJI]* :',  f'\n{res.text}')
+    st.write(f'*:violet[BUJJI]* :',  f'\n{res.text}')
     # res_text = res.text
     # st.session_state.loop +=1
     # st.session_state.chats.append({'user':user_query, 'response':res_text})
